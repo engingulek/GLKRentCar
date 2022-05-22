bb740cd6a7ffb1648a05a6cf4511ec1a375ee757
Engin Gülek <engingulek0@gmail.com>
2022-05-22 12:37:58 +0300

    FirstCommit:CreateProject

---
+++ GLKRentCar/GLKRentCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GLKRentCar
+//
+//  Created by engin gülek on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
