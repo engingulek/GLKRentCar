00ee82ed54611d5c4afabd83be515f6ae9616961
Engin Gülek <engingulek0@gmail.com>
2022-05-22 16:15:57 +0300

    ConnectFirebaseAuth

--- GLKRentCar/GLKRentCar/view/CreateAccount.swift
+++ GLKRentCar/GLKRentCar/view/CreateAccount.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 
 class CreateAccount : UIViewController {
@@ -19,16 +20,40 @@ class CreateAccount : UIViewController {
         
     }
     
-}
 
-
-
-
-// uıDesign
-extension CreateAccount {
-    func uiDesign(){
+    // create account function
+    @IBAction func createAccount(_ sender: Any) {
         
-        
+        if (self.emailTextField.text == "" || self.passwordTextField.text == "" || self.nameTextField.text == ""){
+            self.alertMessage(alertTitle: "Error", alertMessage: "There is free empty")
+            
+        }else {
+            Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { authResult, error in
+                if error != nil {
+                    
+                }else {
+                    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                    changeRequest?.displayName = "\(String(describing: self.nameTextField.text))"
+                    self.performSegue(withIdentifier: "toHomePage", sender: nil)
+                                     
+                }
+                
+            }
+            
+        }
+       
     }
     
+    /// error alert message
+    func alertMessage(alertTitle:String,alertMessage:String)
+    {
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Oket", style: .cancel)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
+    
+    
 }
+
+
--- GLKRentCar/GLKRentCar/view/LogInPage.swift
+++ GLKRentCar/GLKRentCar/view/LogInPage.swift
@@ -8,11 +8,16 @@
 import Foundation
 import UIKit
 
+
+import FirebaseAuth
+
 class LogInPage : UIViewController {
     @IBOutlet weak var emailTextField : UITextField!
     @IBOutlet weak var passwordTextField : UITextField!
-    @IBOutlet weak var googleButton : UIButton!
+    
     @IBOutlet weak var creatAccountToLabeL: UILabel!
+
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         uıSetup()
@@ -24,14 +29,13 @@ class LogInPage : UIViewController {
         self.creatAccountToLabeL.addGestureRecognizer(tap)
     }
     
-    
-    
+
     @objc func toCreatePage(_ sender:Any){
       
         performSegue(withIdentifier: "toCreateAccountPage", sender: nil)
-     
     }
     
+
     
     func uıSetup() {
         // emailTextField
@@ -45,18 +49,40 @@ class LogInPage : UIViewController {
         self.passwordTextField.layer.borderColor = UIColor.lightGray.cgColor
         self.passwordTextField.layer.cornerRadius = 10
         self.passwordTextField.isSecureTextEntry = true
-        
-        
-        
-        
-        
-        
-        
-        
-        
-       
-        
     }
+    
+    
+  
+}
+
+// LogIn
+extension LogInPage {
+    @IBAction func loginButtonAction(_ sender: Any) {
+        if (self.emailTextField.text == "" || self.passwordTextField.text == ""){
+            self.alertMessage(message: "There is free space", title: "Error")
+        }else {
+            Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!){
+                authResult,error in
+                
+                if let error = error {
+                    self.alertMessage(message: error.localizedDescription, title: "Error")
+                    
+                }else{
+                    self.performSegue(withIdentifier: "toHomePage", sender: nil)
+                }
+            }
+        }
+    }
+    
+    
+    // Error alert message
+    func alertMessage(message:String,title:String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Okey", style: .cancel)
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true)
+    }
+    
 }
 
 
